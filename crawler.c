3e2b06f387e588eda90d32650a846061f9fb20b5
c0d3Gems <techdubberstudio@gmail.com>
2018-01-10 10:37:02 -0500

    Custom write function added

--- crawler.c
+++ crawler.c
@@ -17,6 +17,10 @@
 		#define NY_TIMES_URL 	(const char*)"https://www.nytimes.com/"
 		#define WSJ_URL 		(const char*)"https://www.wsj.com/"
 
+
+		#define NY_TIMES_INDEX_FILE		(const char*)"nytimes.html"
+		#define WSJ_TIMES_INDEX_FILE	(const char*)"wsj.html"
+
 struct string
 {
 	char* content;
@@ -28,6 +32,7 @@ void stringInit(struct string *s)
 {
 	s->length=0;
 	s->content=malloc(s->length + 1);
+	memset(s->content, '\0', s->length+1);
 	if(s->content==NULL)
 	{
 		printf("malloc() failed\n");
@@ -43,6 +48,7 @@ size_t writeFunction(void *ptr, size_t size, size_t nmemb, struct string *s)
 	free(s->content);
 	s->content=NULL;
 	s->content=malloc(new_len+1);
+	memset(s->content, '\0', new_len+1);
 
 	if(s->content==NULL)
 
@@ -67,18 +73,46 @@ char* simpleGetRequest(const char* url)
 	curl = curl_easy_init();
 	if(curl)
 	{
-		curl_easy_setopt(curl, CURLOPT_URL, url);
+		struct string s;
+		stringInit(&s);
 
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); 
+		curl_easy_setopt(curl, CURLOPT_URL, url); //provide the url link we want to visit
 
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		// curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction); //call a custom writefunction instead of fwrite
+
+		// curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);//where to write the output 
+
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // do not check for the ssl certificate authenticity 
+
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);	// allow redirections
 
 		res = curl_easy_perform(curl);
 		if(res!=CURLE_OK)
 			printf("curl_easy_perform() failed with error code: %s\n", curl_easy_strerror(res));
 		curl_easy_cleanup(curl);
 		
-		return (char*) res;
+
+
+		
+		s.content[s.length]='\0';
+		
+		FILE *outputFile = fopen(NY_TIMES_INDEX_FILE, "w");
+		
+		fwrite(s.content, s.length, 1, outputFile);
+		
+		fclose(outputFile);
+		
+		outputFile=NULL;
+		
+
+
+
+		printf("Size is: %llu bytes\n", (unsigned long long )s.length);
+
+
+		return (char*) &s.content[0];
 	}
 	return (char*) NULL;
 }
@@ -164,9 +198,9 @@ int main(int argc, char **argv)
 {
 
 	printf("Hello, world!\n");
-	char *rawContentNyTimes=simpleGetRequest("https://www.nytimes.com");
+	char *rawContentNyTimes=simpleGetRequest(NY_TIMES_URL);
 
-	//printf("%s\n", rawContentNyTimes);
+	// printf("%s\n", rawContentNyTimes);
 	// printf("%llu\n", (unsigned long long )strlen(rawContentNyTimes));
 	free(rawContentNyTimes);
 	return 0;
